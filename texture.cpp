f9c2470c7a02c2a4cc85f51d81afedbbbe234c39
LittleTheFu <lttlstpd@live.com>
2025-04-30 18:01:38 +0800

    implement Texture class with constructor and getColor method

--- texture.cpp
+++ texture.cpp
@@ -1,1 +1,29 @@
 #include "texture.h"
+#include <iostream>
+
+Texture::Texture(const std::string &fileName) : m_width(0), m_height(0)
+{
+     // decode
+     unsigned error = lodepng::decode(m_data, m_width, m_height, fileName.c_str());
+
+     // if there's an error, display it
+     if (error)
+         std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+}
+
+void Texture::getColor(float u, float v, uint8_t& r, uint8_t& g, uint8_t& b) const
+{
+    //something wrong here, uv passed was out of range
+    //should be fixed later
+    float uu = u - (int)u;
+    float vv = v - (int)v;
+
+    uint8_t uPixel = (static_cast<uint8_t>(uu * m_width));
+    uint8_t vPixel = (static_cast<uint8_t>(vv * m_height));
+
+    uint8_t index = vPixel * m_width + uPixel;
+
+    r = m_data.at(4 * index + 0) / 255.0f;
+    g = m_data.at(4 * index + 1) / 255.0f;
+    b = m_data.at(4 * index + 2) / 255.0f;
+}
--- texture.h
+++ texture.h
@@ -1,9 +1,19 @@
 #ifndef _TEXTURE_H_
 #define _TEXTURE_H_
 
+#include <string>
+#include <vector>
 #include <lodepng/lodepng.h>
 
 class Texture {
+public:
+    Texture(const std::string& fileName); // 补充构造函数声明
+    void getColor(float u, float v, uint8_t& r, uint8_t& g, uint8_t& b) const; // 补充getColor方法声明
+
+private:
+    std::vector<unsigned char> m_data;
+    unsigned m_width;
+    unsigned m_height;
 };
 
-#endif
+#endif // _TEXTURE_H_
