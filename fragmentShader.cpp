6e0d5aa3de852e91c284919765aa079a9d248f29
LittleTheFu <lttlstpd@live.com>
2025-05-02 15:08:48 +0800

    blend vertex color with texture color

--- fragmentShader.cpp
+++ fragmentShader.cpp
@@ -20,8 +20,13 @@ void FragmentShader::apply(const Vertex &vertex)
 
     float u = vertex.texCoord.x();
     float v = vertex.texCoord.y();
-    uint8_t r, g, b;
-    texture_.getColor(u, v, r, g, b);
+
+    float r = vertex.color.x();
+    float g = vertex.color.y();
+    float b = vertex.color.z();
+
+    uint8_t texture_r, texture_g, texture_b;
+    texture_.getColor(u, v, texture_r, texture_g, texture_b); // 获取纹理颜色
     
     Eigen::Vector3f ambient = light_.ambient();
     Eigen::Vector3f diffuse = light_.diffuse(vertex.worldPosition, vertex.normal);
@@ -29,13 +34,11 @@ void FragmentShader::apply(const Vertex &vertex)
     Eigen::Vector3f finalColor = ambient + diffuse + specular; // 计算最终颜色
 
     finalColor = finalColor.cwiseProduct(Eigen::Vector3f{static_cast<float>(r), static_cast<float>(g), static_cast<float>(b)});
+    finalColor = finalColor.cwiseProduct(Eigen::Vector3f{static_cast<float>(texture_r), static_cast<float>(texture_g), static_cast<float>(texture_b)}); // 乘以纹理颜色
     finalColor = finalColor.cwiseMax(0.0f).cwiseMin(255.0f); // 限制颜色范围在 0-255 之间
 
-    r = static_cast<uint8_t>(finalColor.x());
-    g = static_cast<uint8_t>(finalColor.y());
-    b = static_cast<uint8_t>(finalColor.z());
-
-    if(!frameBuffer_.testAndUpdate(vertex.position.x(), vertex.position.y(), vertex.position.z(), r, g, b, 255))
+    if (!frameBuffer_.testAndUpdate(vertex.position.x(), vertex.position.y(), vertex.position.z(),
+                                    finalColor.x(), finalColor.y(), finalColor.z(), 255)) // 更新帧缓冲区
     {
         return; // 更新失败，返回
     }
