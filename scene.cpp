23b2373720ed400a40168eb9b1324abe695416f9
LittleTheFu <lttlstpd@live.com>
2025-05-02 16:19:58 +0800

    add a triangle into the scene

--- scene.cpp
+++ scene.cpp
@@ -2,7 +2,7 @@
 #include "triangle.h"
 
 Scene::Scene(int width, int height)
-    : camera(Eigen::Vector3f(0.0f, 0.0f, -10.0f), // position
+    : camera(Eigen::Vector3f(0.0f, 0.0f, -30.0f), // position
              Eigen::Vector3f(0.0f, 0.0f, 1.0f),   // target
              Eigen::Vector3f(0.0f, 1.0f, 0.0f)),  // up
       mesh("teapot.obj"),
@@ -10,8 +10,8 @@ Scene::Scene(int width, int height)
       frameBuffer(width, height),
       pipeline(width, height, frameBuffer, light)
 {
-    texture = std::make_shared<Texture>("lena.png"); // 创建纹理对象
-    pipeline.setTexture(texture); // 设置纹理
+    // texture = std::make_shared<Texture>("lena.png"); // 创建纹理对象
+    // pipeline.setTexture(texture); // 设置纹理
 
     const std::vector<Vertex> &vertices = mesh.getVertices();
     for (auto vertex : vertices)
@@ -20,6 +20,33 @@ Scene::Scene(int width, int height)
         vertex.viewDir = (camera.position - vertex.position.head<3>()).normalized(); // 计算视线方向
         vertexBuffer.addVertex(vertex);
     }
+
+    Vertex v0{Eigen::Vector4f(-30, -30, 60, 1),
+              Eigen::Vector3f(1, 1, 1),
+              Eigen::Vector2f(0, 0),
+              Eigen::Vector3f(0, 0, -1),
+            };
+    v0.worldPosition = v0.position.head<3>(); // 世界坐标
+    v0.viewDir = (camera.position - v0.position.head<3>()).normalized(); // 计算视线方向
+    vertexBuffer.addVertex(v0); // 添加顶点到缓冲区
+
+    Vertex v1{Eigen::Vector4f(-30, 30, 60, 1),
+              Eigen::Vector3f(1, 1, 1),
+              Eigen::Vector2f(1, 0),
+              Eigen::Vector3f(0, 0, -1),
+            };
+    v1.worldPosition = v1.position.head<3>(); // 世界坐标
+    v1.viewDir = (camera.position - v1.position.head<3>()).normalized(); // 计算视线方向
+    vertexBuffer.addVertex(v1); // 添加顶点到缓冲区
+
+    Vertex v2{Eigen::Vector4f(40, -10, 60, 1),
+              Eigen::Vector3f(1, 1, 1),
+              Eigen::Vector2f(0, 1),
+              Eigen::Vector3f(0, 0, -1),
+            };
+    v2.worldPosition = v2.position.head<3>(); // 世界坐标
+    v2.viewDir = (camera.position - v2.position.head<3>()).normalized(); // 计算视线方向
+    vertexBuffer.addVertex(v2); // 添加顶点到缓冲区
 }
 
 const Light &Scene::getLight() const
@@ -64,7 +91,7 @@ void Scene::updateLightPosition()
     count += 1;
     count %= 1000;
 
-    light.setPosition(Eigen::Vector3f(-100.0f + std::sin(count * 0.5f) * 200.0f,
-                                      -100.0f + std::sin(count * 0.5f) * 200.0f,
+    light.setPosition(Eigen::Vector3f(-100.0f + std::sin(count * 0.05f) * 200.0f,
+                                      -100.0f + std::sin(count * 0.05f) * 200.0f,
                                       -40.0f));
 }
